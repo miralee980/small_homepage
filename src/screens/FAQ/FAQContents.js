45a4587963487ef718663c3a45af29437be7cada
miralee <980mira@gmail.com>
2021-02-08 16:34:59 +0900

    FAQ 버그 수정

--- src/screens/FAQ/FAQContents.js
+++ src/screens/FAQ/FAQContents.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect /*, useContext*/ } from "react";
+// import { observer } from "mobx-react";
+// import { RootContext } from "../../store/RootStore";
 import InvestmentTab from "./InvestmentTab";
 import QuestionList from "./QuestionList";
 import PageNum from "../../components/PageNum";
 const DEFINE_LIST_NUM = 10;
 
 const FAQContents = ({ searchText }) => {
-	const [questionList, setQuestionList] = useState();
+	const [questionList, setQuestionList] = useState([]);
 	const [totalNum, setTotalNum] = useState(0);
 	const [selNum, setSelNum] = useState(1);
 	const [selFaqList, setFaqList] = useState("all");
 	const [data, setData] = useState([]);
+	// const root = useContext(RootContext);
+	// const { notice, faq } = root;
 
 	useEffect(() => {
 		var url =
@@ -26,31 +30,59 @@ const FAQContents = ({ searchText }) => {
 			redirect: "follow"
 		};
 
-		if (selFaqList === "all") {
-			url =
-				"https://quantec.zendesk.com/api/v2/help_center/ko/categories/900001257903/articles.json";
-		} else if (selFaqList === "investment") {
-			url =
-				"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900000624966/articles.json";
-		} else if (selFaqList === "account") {
-			url =
-				"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900000624986/articles.json";
-		} else if (selFaqList === "order") {
-			url =
-				"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900001620223/articles.json";
-		} else if (selFaqList === "moneypot") {
-			url =
-				"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900000625006/articles.json";
-		}
+		//if (selFaqList === "all") {
+		url =
+			"https://quantec.zendesk.com/api/v2/help_center/ko/categories/900001257903/articles.json?page=1&per_page=99";
 		fetch(url, requestOptions)
 			.then((response) => response.json())
 			.then((result) => {
 				setSelNum(1);
+				// faq.setFaqData(result.articles, result.count, result.next_page);
 				setData(result.articles);
 				setQuestionList(result.articles);
 				setTotalNum(result.count);
 			})
 			.catch((error) => console.log("error", error));
+		//}
+	}, []);
+
+	useEffect(() => {
+		setSelNum(1);
+		if (selFaqList === "all") {
+			// setData();
+			setQuestionList(data);
+			setTotalNum(data.length);
+			// url =
+			// 	"https://quantec.zendesk.com/api/v2/help_center/ko/categories/900001257903/articles.json";
+		} else if (selFaqList === "investment") {
+			let list = data.filter((list) => list.section_id === 900000624966);
+			// setData(list);
+			setQuestionList(list);
+			setTotalNum(list.length);
+			// url =
+			// 	"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900000624966/articles.json";
+		} else if (selFaqList === "account") {
+			let list = data.filter((list) => list.section_id === 900000624986);
+			// setData(list);
+			setQuestionList(list);
+			setTotalNum(list.length);
+			// url =
+			// 	"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900000624986/articles.json";
+		} else if (selFaqList === "order") {
+			let list = data.filter((list) => list.section_id === 900001620223);
+			// setData(list);
+			setQuestionList(list);
+			setTotalNum(list.length);
+			// url =
+			// 	"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900001620223/articles.json";
+		} else if (selFaqList === "moneypot") {
+			let list = data.filter((list) => list.section_id === 900000625006);
+			// setData(list);
+			setQuestionList(list);
+			setTotalNum(list.length);
+			// url =
+			// 	"https://quantec.zendesk.com/api/v2/help_center/ko/sections/900000625006/articles.json";
+		}
 	}, [selFaqList]);
 
 	useEffect(() => {
